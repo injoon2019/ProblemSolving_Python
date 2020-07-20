5e5ab42a627cf225dba7924ee2df55d9f40301ab
Injun Son <ison@sfu.ca>
2020-07-20 16:37:12 +0900

    July 20, 16:37

--- baekjoon_10989.py
+++ baekjoon_10989.py
@@ -3,9 +3,16 @@ Problem Solving Baekjoon 10989
 Author: Injun Son
 Date: July 20, 2020
 '''
+import sys
 
 N = int(input())
-arr = [int(input()) for _ in range(N)]
-arr.sort()
-for i in range(len(arr)):
-    print(arr[i])
+arr = [0]*10001
+
+for i in range(N):
+    x = int(sys.stdin.readline())
+    arr[x]+=1
+
+for i in range(1, 10001):
+    if arr[i]!=0:
+        for c in range(arr[i]):
+            print(i)
