f79735e1780873b25f4f54cabe8d8ead13399db1
Injun <ison@sfu.ca>
2020-10-09 22:32:30 +0900

    Recommended Problems from Hanyang EveryTime

--- leetcode_valid-palindrome.py
+++ leetcode_valid-palindrome.py
@@ -5,6 +5,9 @@ Date: October 9, 2020
 '''
 import sys
 from collections import deque
+from typing import List
+
 sys.setrecursionlimit(100000)
 
 def reverseString(s: List[str])-> None:
+    s.reverse()
