962df97a000e5fcb80a946fa91c25a18475859a6
Injun <ison@sfu.ca>
2020-10-09 17:52:10 +0900

    reverse string

--- leetcode_valid-palindrome.py
+++ leetcode_valid-palindrome.py
@@ -7,15 +7,4 @@ import sys
 from collections import deque
 sys.setrecursionlimit(100000)
 
-def isPalindrome(s: str)-> bool:
-    strs = []
-    for char in s:
-        if char.isalnum():
-            strs.append(char.lower())
-
-    #팰린드롬 여부 판별
-    while len(strs) > 1:
-        if strs.pop(0) != strs.pop():
-            return False
-
-    return True
+def reverseString(s: List[str])-> None:
