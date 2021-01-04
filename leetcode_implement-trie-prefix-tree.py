4d6dc6aea628f0e6e96530646c66216bf489427f
Injun Son <ison@sfu.ca>
2021-01-04 15:04:11 +0900

    implement Trie

---
+++ leetcode_implement-trie-prefix-tree.py
@@ -0,0 +1,51 @@
+'''
+Problem Solving leetcode implement-trie-prefix-tree
+Author: Injun Son
+Date: January 4, 2020
+'''
+import sys
+import collections
+import heapq
+import functools
+import itertools
+import re
+import math
+import bisect
+from typing import *
+
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = {}
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+
+        return True
+
--- leetcode_kth-largest-element-in-an-array.py
+++ leetcode_kth-largest-element-in-an-array.py
@@ -1,7 +1,7 @@
 '''
 Problem Solving leetcode kth-largest-element-in-an-array
 Author: Injun Son
-Date: November 20, 2020
+Date: January 1, 2020
 '''
 import sys
 import collections
@@ -13,15 +13,29 @@ import math
 import bisect
 from typing import *
 
+
 def findKthLargest(nums: List[int], k: int) -> int:
     heap = list()
     for n in nums:
         heapq.heappush(heap, -n)
 
     for _ in range(k):
-        heapq.heapppop(heap)
+        heapq.heappop(heap)
 
     return -heapq.heappop(heap)
 
-print(findKthLargest([3,2,1,5,6,4], 2))
-print(findKthLargest([3,2,3,1,2,4,5,5,6], 4))
+
+# def findKthLargest(nums: List[int], k: int) -> int:
+#     heapq.heapify(nums)
+#
+#     for _ in range(len(nums) - k):
+#         heapq.heappop(nums)
+#
+#     return heapq.heappop(nums)
+
+def findKthLargest(self, nums: List[int], k: int) -> int:
+    return heapq.nlargest(k, nums)[-1]
+
+
+print(findKthLargest([3, 2, 1, 5, 6, 4], 2))
+print(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
